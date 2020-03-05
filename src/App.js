8b562ea83130e256c6bb892d0715cd74ef05169b
nakayama-ko <nkwork874@gmail.com>
2020-03-05 16:46:46 +0900

    Menu消去

--- src/App.js
+++ src/App.js
@@ -2,22 +2,9 @@ import React from 'react';
 import './App.css';
 import { FiHash } from 'react-icons/fi'
 
-/*トップの項目を生成，リスト化 */
-const topContents = ['Home', 'About Me', 'Works', 'Contact'];
-const TopBar = () => {
-  return (
-    topContents.map((number) =>
-      <li className="nav_item">{number}</li>
-    )
-  );
-}
-
 function App() {
   return (
     <div className="App">
-      <div className="TopBar">
-        <ul className="nav"><TopBar /></ul>
-      </div>
       <div className="App-header">
         <FiHash color='orange' /> Nakayama's Portfolio Page
       </div>
